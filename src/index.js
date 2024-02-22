71602ef4949b3924d0f797f3b2fa0d86f9c18039
WarYaar <forsurya02@gmail.com>
2024-02-22 13:16:18 +0530

    Added v1 for tweet model and hashtag model

--- src/index.js
+++ src/index.js
@@ -1,36 +1,10 @@
 const express = require("express");
 const connect = require("./config/database")
-const Comment = require("./models/comment");
-const app = express();
 
-const tweetRepository = require("./repository/tweet-repository");
+const app = express();
 
 app.listen(3000, async () => {
     console.log("server started");
     await connect();
     console.log("Mongo db connected");
-    // const tweet = await Tweet.create({
-    //     content: "third tweet",
-        
-    // });
-
-    // const tweet = await Tweet.find({ userEmail: "a@b.com"});
-
-    const tweetRepo = new tweetRepository();
-    const tweet = await tweetRepo.create({content: 'With hooks now'});
-    console.log(tweet);
-
-    // const tweet = await tweetRepo.getAll(0, 4);
-    // console.log(tweet[0].contentWithEmail);
-
-    // const tweet = await tweetRepo.create({content: "tweet with comment Schema"});
-    // console.log(tweet);
-    // const comment = await Comment.create({content: "comment with comment schema"});
-    // tweet.comments.push(comment);
-    // await tweet.save();
-    // console.log(tweet);
-    // console.log(tweet);
-    // tweet.comments.push({content: "first comment"});
-    // await tweet.save();
-    // console.log(tweet);
 })
